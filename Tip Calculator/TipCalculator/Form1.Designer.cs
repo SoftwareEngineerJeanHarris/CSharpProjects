907ca2d8b2b185a68ac95dbedbf4ac91a84edd39
Jean-Michael Harris <jeanmichael.harris@gmail.com>
2020-09-13 13:39:06 -0400

    Add files via upload

---
+++ Tip Calculator/TipCalculator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TipCalculator
+{
+    partial class tipCalculatorWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.txtBill = new System.Windows.Forms.TextBox();
+            this.txtTip = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bill:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tip in Percentage(%):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total:";
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcBtn.Location = new System.Drawing.Point(15, 70);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(221, 23);
+            this.calcBtn.TabIndex = 3;
+            this.calcBtn.Text = "Calculate Total";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // txtBill
+            // 
+            this.txtBill.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBill.Location = new System.Drawing.Point(136, 6);
+            this.txtBill.Name = "txtBill";
+            this.txtBill.Size = new System.Drawing.Size(100, 22);
+            this.txtBill.TabIndex = 4;
+            // 
+            // txtTip
+            // 
+            this.txtTip.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTip.Location = new System.Drawing.Point(136, 40);
+            this.txtTip.Name = "txtTip";
+            this.txtTip.Size = new System.Drawing.Size(100, 22);
+            this.txtTip.TabIndex = 5;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(133, 102);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(11, 13);
+            this.txtTotal.TabIndex = 7;
+            this.txtTotal.Text = "-";
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorMessage.ForeColor = System.Drawing.Color.Red;
+            this.errorMessage.Location = new System.Drawing.Point(9, 119);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(0, 12);
+            this.errorMessage.TabIndex = 8;
+            this.errorMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tipCalculatorWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 139);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtTip);
+            this.Controls.Add(this.txtBill);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "tipCalculatorWindow";
+            this.Text = "Tip Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.TextBox txtBill;
+        private System.Windows.Forms.TextBox txtTip;
+        private System.Windows.Forms.Label txtTotal;
+        private System.Windows.Forms.Label errorMessage;
+    }
+}
+
---
+++ Tip Calculator/TipCalculator/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipCalculator
+{
+    public partial class tipCalculatorWindow : Form
+    {
+        //Variable declaration.
+        public String bill, tip, total;
+        public Boolean totalError;
+
+
+        public tipCalculatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            totalError = false;
+
+            //Assigns variables
+            setBill(txtBill.Text);
+            setTip(txtTip.Text);
+
+            //Calls the calcTotal method to run the calculation for the total
+            calcTotal(getBill(), getTip());
+
+            //Returns the result to the text field of txtTotal if no errors are found
+            if(totalError==false)
+            {
+                txtTotal.Text = getTotal();
+            }
+            else
+            {
+                txtTotal.Text = "Error";
+            }
+            
+        }
+
+        public String getBill()
+        {
+            return bill;
+        }
+
+        public void setBill(String bill)
+        {
+            this.bill = bill;
+        }
+
+        public String getTip()
+        {
+            return tip;
+        }
+
+        public void setTip(String tip)
+        {
+            this.tip = tip;
+        }
+
+        public String getTotal()
+        {
+            return total;
+        }
+
+        public void setTotal(String total)
+        {
+            this.total = total;
+        }
+
+        public void calcTotal(String bill, String tip)
+        {
+            //Convert variables
+            Double billDouble = 0;
+            Double tipDouble = 0;
+
+            //Attempt to convert varable "bill" from string to double if user input is valid
+            try
+            {
+                billDouble = Convert.ToDouble(bill);
+                totalError = false;
+                try
+                {
+                    tipDouble = Convert.ToDouble(tip) / 100;
+
+                    //Calculates the total based on the bill and tip percentage and then rounds this value up to the last two decimals
+                    Double total = Math.Round(((billDouble * tipDouble) + billDouble), 2, MidpointRounding.ToEven);
+
+                    setTotal(total.ToString());
+                    errorMessage.Text = "";
+                }
+                catch (Exception e)
+                {
+                    if(totalError == false)
+                    {
+                        errorMessage.Text = "Error Found: please enter a 1valid number for the tip.";
+                        totalError = true;
+                    }
+                    else
+                    {
+                        errorMessage.Text = "Error Found: please enter a valid number for the bill and tip.";
+                    }
+                    
+                }
+            }
+            catch(Exception e)
+            {
+                errorMessage.Text = "Error Found: please enter a valid number for the bill.";
+                totalError = true;
+            }
+        }
+    }
+}
